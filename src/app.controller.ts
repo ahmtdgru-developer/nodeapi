e000c6808b770efada5685a0553aadc199a68ac9
ahmetdogru <ahmtdgru@gmail.com>
2026-04-03 16:50:56 +0300

    feat: redirect root endpoint to /api using Redirect decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -8,7 +8,6 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Redirect('/api', 301)
+  getHello() { }
 }
